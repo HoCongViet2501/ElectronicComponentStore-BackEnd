062f7bc365eb65d846c395ca2ea9cf7a966cf26e
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-08-28 17:16:34 +0700

    first commit

---
+++ src/main/java/com/be/electroniccomponentstore/ElectronicComponentStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.be.electroniccomponentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicComponentStoreBackEndApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicComponentStoreBackEndApplication.class, args);
+    }
+    
+}
